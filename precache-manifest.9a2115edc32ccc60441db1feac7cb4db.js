4500409df2a95be404afe252126c2903e8021d25
jonnotung <jonno.tung@gmail.com>
2019-06-02 17:03:44 -0400

    Updates

---
+++ precache-manifest.9a2115edc32ccc60441db1feac7cb4db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad6481635037953bb1a9cbed2181e2d",
+    "url": "/jonnotung/JonathanTungProject5/index.html"
+  },
+  {
+    "revision": "f38d8f5fa99d81cc45ec",
+    "url": "/jonnotung/JonathanTungProject5/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "f9e37d07c5cf78e47c5b",
+    "url": "/jonnotung/JonathanTungProject5/static/css/main.74020954.chunk.css"
+  },
+  {
+    "revision": "f38d8f5fa99d81cc45ec",
+    "url": "/jonnotung/JonathanTungProject5/static/js/2.a9339d80.chunk.js"
+  },
+  {
+    "revision": "f9e37d07c5cf78e47c5b",
+    "url": "/jonnotung/JonathanTungProject5/static/js/main.354ecf39.chunk.js"
+  },
+  {
+    "revision": "3ded9a92029335c8fbf2",
+    "url": "/jonnotung/JonathanTungProject5/static/js/runtime~main.1eedc448.js"
+  }
+]);
