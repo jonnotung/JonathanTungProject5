75de6cf1e2a3da3032cecbdc3c8af0078b1ba0a6
jonnotung <jonno.tung@gmail.com>
2019-06-02 17:12:19 -0400

    added favicon

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => {
     return(
         <header className="intro">
-            <h1>School Database</h1>
+            <h1>Class Database</h1>
             <p>Enter the name of the student. (Max 25 characters)</p>
             <p>Enroll the classes that they're in. Class names should of the form (3 letters)(3 numbers). Click on an enrolled class to remove it.</p>
             <p>Click Create Entry to save to database.</p>
