31437f1b83279482bd412a5c2797b97503d20472
Jonathan Tung <jonno.tung@gmail.com>
2019-06-20 12:38:16 -0400

    text resizing

--- src/conflictMethods.js
+++ src/conflictMethods.js
@@ -1,11 +1,15 @@
 class ConflictMethods {
 
     //argument: object with {name: [enrolled classes]}
-    //returns 
-    getConflictGroups = (entries) => {
+    //returns a graph where verticies = classes, edges = a student is taking classes that the edge connnects
+    getConflictGraph = (entries) => {
+        const graph = {};
         for(let key in entries) {
-            
+            for(let i = 0; i < entries[key].length; i++) {
+                
+            }
         }
+        return graph;
     }
 
 
